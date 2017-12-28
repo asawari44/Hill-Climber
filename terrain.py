7abfc98af102a7af7a9ce18b6f3218226e287cfb
kartikchincholikar <kartikchincholikar5@gmail.com>
2017-12-28 12:35:35 +0530

    Version 2
    It is barely working.Need to make it readable

--- terrain.py
+++ terrain.py
@@ -12,14 +12,12 @@ def get_basic_terrain(size=100):
 
     return terrain
 
-def get_sine_terrain(size=100):
-    Fs = 100
-    f = 2
-    sample = 100
+def get_sine_terrain(size=100,sample=1000,f=2):
+    Fs = size
     x = np.arange(sample)
     y = np.sin(2 * np.pi * f * x / Fs)
     
-    return y.reshape(1,size)
+    return y.reshape(1,sample)
 
 def get_basic_terrain_descending(size=100):
     x = np.arange( size,0, -1)
@@ -31,3 +29,24 @@ def get_basic_terrain_descending(size=100):
     terrain.shape[1]
 
     return terrain
+
+def terrain_complex(sample=1000):
+    
+    sin = get_sine_terrain(size=200,sample=1000,f=.5)
+
+    sin1 = get_sine_terrain(size=250,sample=1000,f=.4)
+
+    sin2 = get_sine_terrain(size=500,sample=1000,f=.6)
+
+    terrain =  sin+sin1+sin2
+    
+    return terrain
+
+def get_parabola(sample = 1000):
+    
+    x= np.arange(-sample/2,sample/2)
+    y = 2*x*x + 4*x
+
+    terrain =  y
+    
+    return terrain.reshape(1,sample)
