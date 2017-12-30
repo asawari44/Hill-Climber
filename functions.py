43d4e78aa43044338d6cb397aa4acf44a4d3fb09
kartikchincholikar <kartikchincholikar5@gmail.com>
2017-12-30 15:32:51 +0530

    Backin up files

--- functions.py
+++ functions.py
@@ -5,3 +5,14 @@ def sigmoid(z):
 
 def sigmoid_prime(z):
     return sigmoid(z)*(1-sigmoid(z))
+
+
+def reset_climber(climber):
+    climber.position_index =np.random.randint(climber.step,climber.terrain.shape[1]-climber.n_input-climber.step-1)#i%975 + 3 #
+    climber.old_input = climber.terrain[0,climber.position_index:climber.position_index + climber.n_input].reshape(climber.n_input,1)
+    climber.input = climber.terrain[0,climber.position_index:climber.position_index + climber.n_input].reshape(climber.n_input,1)
+    climber.output = None
+    climber.y = 0
+    
+    climber.position_switch_counter = climber.position_switch_counter + 1
+    print("-------NEW POSITION-------")
--- terrain.py
+++ terrain.py
@@ -1,16 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def get_basic_terrain(size=100):
-    x = np.arange(0, size, 1)
-
-    noise = np.random.normal(scale=3, size=[1, size])
-
-    terrain = x + noise
-
-    terrain.shape[1]
-
-    return terrain
+def get_basic_terrain(sample=1000,std = 3):
+    x = np.arange(0, sample, 1)
+    terrain = x 
+    return terrain.reshape(1,sample)
 
 def get_sine_terrain(size=100,sample=1000,f=2):
     Fs = size
@@ -19,34 +13,34 @@ def get_sine_terrain(size=100,sample=1000,f=2):
     
     return y.reshape(1,sample)
 
-def get_basic_terrain_descending(size=100):
-    x = np.arange( size,0, -1)
+def get_basic_terrain_descending(sample=1000):
+    x = np.arange(sample,0, -1)
+    terrain = x 
 
-    noise = np.random.normal(scale=3, size=[1, size])
+    return terrain.reshape(1,sample)
 
-    terrain = x + noise
 
-    terrain.shape[1]
-
-    return terrain
-
-def terrain_complex(sample=1000):
-    
-    sin = get_sine_terrain(size=200,sample=1000,f=.5)
-
-    sin1 = get_sine_terrain(size=250,sample=1000,f=.4)
-
-    sin2 = get_sine_terrain(size=500,sample=1000,f=.6)
-
-    terrain =  sin+sin1+sin2
-    
-    return terrain
-
-def get_parabola(sample = 1000):
+def get_parabola(sample = 1000,a=2,b=4):
     
     x= np.arange(-sample/2,sample/2)
-    y = 2*x*x + 4*x
+    y = a*x*x + b*x
 
     terrain =  y
     
     return terrain.reshape(1,sample)
+
+def get_random_terrain(sample = 1000):
+    a = 0
+    for i in range(100):
+        a = a + np.random.randint(-3,3,1)*get_sine_terrain(sample/10,sample = sample,f = np.random.normal(1,2)/10)
+
+    terrain = a/100
+    return terrain.reshape(1,sample)
+
+def get_noise(sample = 1000,std=3):
+    noise = np.random.normal(scale=std, size=[1, sample])
+    return noise
+
+
+
+    
